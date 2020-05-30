86b27db4c14fda399d104c13ec29e5c031d1ea49
unknown <robbi.mobile@gmail.com>
2020-05-30 18:47:48 +0200

    initial java11 project for simple gradle config

--- src/main/java/guru/springframework/sfgpetclinic/controllers/ValueNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package guru.springframework.sfgpetclinic.controllers;
-
-/**
- * Created by jt on 2018-10-22.
- */
-public class ValueNotFoundException extends RuntimeException {
-}
--- src/main/java/guru/springframework/sfgpetclinic/model/OwnerType.java
+++
@@ -1,9 +0,0 @@
-package guru.springframework.sfgpetclinic.model;
-
-/**
- * Created by jt on 2018-10-28.
- */
-public enum OwnerType {
-
-    INDIVIDUAL, COMPANY
-}
